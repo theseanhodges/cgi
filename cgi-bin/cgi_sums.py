5fea5ef0cc519561d1fb9214489195c650ea91bd
Sean Hodges <sthodges@gmail.com>
2021-03-15 16:33:08 -0700

    Implement sum

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,15 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+operands = form.getlist('operand')
+
 print("Content-type: text/plain")
-print()
-print("Your job is to make this work")
+print("")
+
+try:
+    operand_sum = sum([int(i) for i in operands])
+except:
+    print("One or more operands is invalid.  Only integers are acceptable.")
+else:
+    print("Sum of operands: {}".format(operand_sum))
